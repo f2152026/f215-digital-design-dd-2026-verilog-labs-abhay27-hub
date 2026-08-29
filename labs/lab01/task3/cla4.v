ae74cdbe1d5a3984b6b274effc15019e3a93bb4d
Abhay27-hub <f20250743@pilani.bits-pilani.ac.in>
2026-08-29 11:33:13 +0000

    lab01 task3

--- labs/lab01/task3/cla4.v
+++ labs/lab01/task3/cla4.v
@@ -29,9 +29,50 @@ module cla4(
 
   wire p0, p1, p2, p3;
   wire g0, g1, g2, g3;
-  wire c1, c2, c3;
+  wire c1, c2, c3, c0, c4;
 
   // TODO: your gate-level P/G, carry, and sum logic goes here.
+
+  assign p0 = a[0] ^ b[0] ;
+  assign g0 = a[0] & b[0] ;
+  assign p1 = a[1] ^ b[1] ;
+  assign g1 = a[1] & b[1] ;
+  assign p2 = a[2] ^ b[2] ;
+  assign g2 = a[2] & b[2] ;
+  assign p3 = a[3] ^ b[3] ;
+  assign g3 = a[3] & b[3] ;
+  
   // (cout should be connected to c4.) Remember the delay on every gate.
+  // --- Carry c1 ---
+wire t1_0;
+and #(2) (t1_0, p[0], cin);
+or  #(2) (c[1], g[0], t1_0);
+
+// --- Carry c2 ---
+wire t2_0, t2_1;
+and #(2) (t2_0, p[1], g[0]);
+and #(2) (t2_1, p[1], p[0], cin);
+or  #(2) (c[2], g[1], t2_0, t2_1);
+
+// --- Carry c3 ---
+wire t3_0, t3_1, t3_2;
+and #(2) (t3_0, p[2], g[1]);
+and #(2) (t3_1, p[2], p[1], g[0]);
+and #(2) (t3_2, p[2], p[1], p[0], cin);
+or  #(2) (c[3], g[2], t3_0, t3_1, t3_2);
+
+// --- Carry c4 ---
+wire t4_0, t4_1, t4_2, t4_3;
+and #(2) (t4_0, p[3], g[2]);
+and #(2) (t4_1, p[3], p[2], g[1]);
+and #(2) (t4_2, p[3], p[2], p[1], g[0]);
+and #(2) (t4_3, p[3], p[2], p[1], p[0], cin);
+or  #(2) (c[4], g[3], t4_0, t4_1, t4_2, t4_3);
+
+assign sum[1] = p1 ^ c1 ;
+assign sum[2] = p2 ^ c2 ;
+assign sum[3] = p3 ^ c3 ;
+assign c0 = cin; 
+assign c4 = cout; 
 
 endmodule
